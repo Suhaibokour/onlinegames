3a99b1e6d0994920931b9cb4094b39278451744d
suhaibokour <>
2021-06-16 14:25:10 +0000

    linked js to html file

---
+++ app.js
@@ -0,0 +1,10 @@
+var name = prompt("Write your name to confirm that you are not a Robot please") 
+
+alert("WELCOME to Dota2 introduction " + name)
+
+var knowdota2 = prompt("Have you ever played Dota2 before?( yes . no )" )
+
+if(knowdota2 == "no"){alert("Then read down below to learn about it")}
+else{ alert("Then add me on steam by the name Zarakiok")} 
+
+//Hello this is suhaib and i belive this is the theird dynamic component to finish with, thank you. 
